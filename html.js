9b7c9a85b09d5ce1421c000931d208eae5b4e116
Hyunseung <hyunseunglee2008@gmail.com>
2023-10-21 11:57:56 +0900

    update style, directories

--- html.js
+++ html.js
@@ -4,13 +4,13 @@ function returnEQ(marker) {
 
   return `
   
-    <div style="" class="overlay">
+    <div class="overlay">
       <a href="${marker.link}">
         <p class="title">${title}</p>
         <p class="time">${marker.time}</p>
       </a>
       
-      <a href="https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/prevent09.html?menuSeq=126">
+      <a class="howtoact" href="https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/prevent09.html?menuSeq=126">
         <p>행동요령</p>
       </a>
     </div>
