8482cba4a9ae05f08d2d6879676c2d61644ff69e
Hyunseung <hyunseunglee2008@gmail.com>
2023-10-22 18:10:57 +0900

    finish react setup

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,7 @@
 import { useKakaoLoader, Map, MapMarker } from "react-kakao-maps-sdk";
 import { useEffect, useState } from "react";
-import "./App.css";
-
-import { returnEQ } from "./utils/overlay.js";
+import ReactLoading from "react-loading";
+import "./css/App.css";
 
 function App() {
   const [loading, error] = useKakaoLoader({
@@ -28,23 +27,61 @@ function App() {
           const longitude = parseFloat(res[i].info.longitude);
           const latitude = parseFloat(res[i].info.latitude);
 
+          const title = res[i].title;
+          const source = res[i].source;
+          const time = res[i].time;
+          const link = res[i].link;
+
           setMarkers((markers) => [
             ...markers,
-            { lat: latitude, lng: longitude, text: returnEQ(res[i]) },
+            { lat: latitude, lng: longitude, title, source, time, link },
           ]);
+
+          setMarkers((markers) => {
+            const seen = new Set();
+            const filteredArr = markers.filter((el) => {
+              const duplicate = seen.has(el.lat + el.lng);
+              seen.add(el.lat + el.lng);
+              return !duplicate;
+            });
+            return filteredArr;
+          });
         }
       });
-  });
+  }, []);
+
+  if (loading)
+    return (
+      <div className="spinner-wrapper">
+        <ReactLoading type="spin" color="skyblue" height={300} width={300} />
+      </div>
+    );
 
   return (
     <div className="App">
       <Map
         center={{ lat: lat, lng: lng }}
         style={{ width: "100%", height: "100vh" }}
+        level={12}
       >
         <MapMarker position={{ lat: lat, lng: lng }}></MapMarker>
+
         {markers.map((marker, index) => (
-          <MapMarker position={{ lat: marker.lat, lng: marker.lng }}></MapMarker>
+          <MapMarker
+            clickable
+            position={{ lat: marker.lat, lng: marker.lng }}
+            key={`${marker.lat}/${marker.lng}/${index}`}
+            image={{
+              src: "/img/earthquake.png",
+              size: { width: 40, height: 45 },
+            }}
+            onClick={() => window.open(marker.link)}
+          >
+            <div onClick={() => window.open(marker.link)} className="overlay">
+              <p className="title">{marker.title?.slice(7)}</p>
+              <p className="time">{marker.time}</p>
+            </div>
+          </MapMarker>
         ))}
       </Map>
     </div>
